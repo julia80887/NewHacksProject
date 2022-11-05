20097285c88b6939673eef381d4e7fa35624bb99
Julia Bulat <juliabulat77@gmail.com>
2022-11-05 13:59:53 -0400

    initial commit

---
+++ Backend/ClosetApplication/src/main/java/com/example/ClosetApplication/Repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.ClosetApplication.Repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import com.example.ClosetApplication.Documents.User;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
---
+++ Backend/ClosetApplication/src/main/java/com/example/ClosetApplication/Service/UserService.java
@@ -0,0 +1,8 @@
+package com.example.ClosetApplication.Service;
+
+import com.example.ClosetApplication.Documents.User;
+
+public class UserService {
+
+
+}
