690a6d5a61944b8e9b1bd0157f9fab4e519941ab
rumaisachowdhury <rumaisa.chowdhury@mail.utoronto.ca>
2022-11-06 01:46:11 -0400

    Create input fields for Login Page

--- NewHacksApp/screens/Login.js
+++ NewHacksApp/screens/Login.js
@@ -1,11 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
-// import { StackActions } from '@react-navigation/native';
+import React, { FC, ReactElement, useState } from "react";
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+//import { StackActions } from '@react-navigation/native';
 
 export default function Login({ navigation}) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   return (
     <>
+      <Text>Enter username:</Text>
+      <TextInput
+        style={styles.input}
+        value={username}
+        placeholder={"Username"}
+        onChangeText={(text) => setUsername(text)}
+        autoCapitalize={"none"}
+      />
+      <Text>Enter password:</Text>
+      <TextInput
+        style={styles.input}
+        value={password}
+        placeholder={"Password"}
+        secureTextEntry
+        onChangeText={(text) => setPassword(text)}
+      />
+
+      
       <Button 
         title='Go to Sign Up'
         onPress={() => navigation.navigate("Signup")}
@@ -37,4 +58,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  navigate: {
+    //flex: 1,
+    borderwidth: 1,
+    borderColor: '#777',
+    padding: 8,
+    margin: 10,
+    width:200,
+    bottom: 5,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  
 });
